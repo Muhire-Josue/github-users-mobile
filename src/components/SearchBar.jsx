63cfb119ca31e585ebed251d58417f24f08c865d
Muhire-Josue <rutayisire.josue09@gmail.com>
2022-02-28 20:13:04 +0200

    implementing search bar UI on home screen

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,17 +2,22 @@ import react from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-export const SearchBar = () => {
+export const SearchBar = ({term, onTermChange}) => {
   return (
-    <View style={styles.background}>
-      <Feather name="search" size={30} />
-      <TextInput style={styles.inputStyle} placeholder='Search...'/>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconsStyle} />
+      <TextInput style={styles.inputStyle} placeholder="Search..."
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+        onTermSubmit={() => console.log('Term was submitted')}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
+    marginTop: 10,
     backgroundColor: "#D1D1D1",
     height: 50,
     borderRadius: 5,
@@ -20,7 +25,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   inputStyle: {
-    borderColor: "black",
-    borderWidth: 1,
+    flex: 1,
+    fontSize: 18
+  },
+  iconsStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
   },
 });
--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,12 +1,15 @@
-import react from 'react';
+import react, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { SearchBar } from "../components/SearchBar";
-
+import github from '../api/github';
 export const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [users, setUsers] = useState([]);
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={users} onTermChange={(newTerm) => setTerm(newTerm)} />
       <Text>Search....</Text>
+      <Text>{users.length}</Text>
     </View>
-  )
+  );
 }
